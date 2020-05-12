28b0579b1d9cc1b6b0f6dee4769a2117e4c2ef54
徐松 <pinesongcn@qq.com>
2020-05-12 09:50:10 +0800

    no message

--- FhMonkey-屏蔽微博广告.js
+++ FhMonkey-屏蔽微博广告.js
@@ -2,7 +2,7 @@
 // @reminder        请不要删除这部分代码注释，这是FeHelper油猴脚本能正常工作的基本条件！当然，你可以按需修改这里的内容！
 // @id              mf_1589247144808
 // @name            屏蔽微博广告
-// @url-pattern     https://*.weibo.com/*
+// @url-pattern     https://*weibo.com/*
 // @enable          true
 // @require-js      
 // @auto-refresh    0
