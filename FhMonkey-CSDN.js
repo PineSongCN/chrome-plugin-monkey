b4ee295be9a0773a9bcf3bc743c9cc4b0afb7c51
徐松 <pinesongcn@qq.com>
2020-05-15 18:10:27 +0800

    no message

---
+++ FhMonkey-CSDN.js
@@ -0,0 +1,21 @@
+// ==FeHelperMonkey==
+// @reminder        请不要删除这部分代码注释，这是FeHelper油猴脚本能正常工作的基本条件！当然，你可以按需修改这里的内容！
+// @id              mf_1589536518465
+// @name            CSDN
+// @url-pattern     https://blog.csdn.net/*
+// @enable          true
+// @require-js      
+// @auto-refresh    0
+// @updated         2020-05-15 18:08:53
+// ==/FeHelperMonkey==
+
+
+(() => {
+    let title = document.title;
+    title = title.indexOf('条消息)') === 2 ? title.split('条消息)')[1] : 0;
+    document.title = title;
+    document.querySelector('#msg-circle').remove();
+
+    document.querySelector('.hide-article-box').remove();
+    document.querySelector('#article_content').style.height = 'auto';
+})();
