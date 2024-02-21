5122ecd803d8adc59914708540512a8368bd2a80
Song Xu <39976617+PineSongCN@users.noreply.github.com>
2024-02-21 13:12:55 +0800

    Update FhMonkey-贝壳.js
    成交记录

--- FhMonkey-贝壳.js
+++ FhMonkey-贝壳.js
@@ -2,7 +2,7 @@
 // @reminder        请不要删除这部分代码注释，这是FeHelper油猴脚本能正常工作的基本条件！当然，你可以按需修改这里的内容！
 // @id              mf_1706593652801
 // @name            贝壳
-// @url-pattern     https://*.ke.com/ershoufang/*
+// @url-pattern     https://*.ke.com/*
 // @enable          true
 // @require-js      
 // @auto-refresh    0
@@ -11,20 +11,40 @@
 
 
 (() => {
-  var total = 0;
-  var box = document.querySelector("#infoList");
+    const pathname = location.pathname;
+    if (pathname.startsWith('/ershoufang/')) {
+        var total = 0;
+        var box = document.querySelector('#infoList');
 
-  if(box) {
-    var rows = Object.values(box.querySelectorAll(".row"));
-    for (const row of rows) {
-      var cols = Object.values(row.querySelectorAll(".col"));
-      total += parseFloat(cols[1].innerText);
+        if (box) {
+            var rows = Object.values(box.querySelectorAll('.row'));
+            for (const row of rows) {
+                var cols = Object.values(row.querySelectorAll('.col'));
+                total += parseFloat(cols[1].innerText);
+            }
+            total = Number(total.toFixed(2));
+            console.log(total);
+
+            var target = document.querySelector(
+                '.introContent .content li:nth-child(2)'
+            );
+            var current = parseFloat(target.innerText.match(/\d+(\.\d+)?/)[0]);
+            target.appendChild(
+                document.createTextNode(
+                    `/ ${total}㎡ | ${Number(
+                        (((current - total) / current) * 100).toFixed(2)
+                    )}%`
+                )
+            );
+        }
     }
-    total = Number(total.toFixed(2))
-    console.log(total);
-    
-    var target = document.querySelector('.introContent .content li:nth-child(2)')
-    var current = parseFloat(target.innerText.match(/\d+(\.\d+)?/)[0]);
-    target.appendChild(document.createTextNode(`/ ${total}㎡ | ${Number(((current-total) / current * 100).toFixed(2))}%`))
-  }
+
+    if (pathname.startsWith('/xiaoqu/')) {
+        var cjjl = document.querySelector('#frameDeal .frameDealHeader');
+        if (cjjl) {
+            cjjl.innerHTML = `<a href='/chengjiao/c${location.pathname.split('/')[2]
+                }/' target='_blank'>${cjjl.innerText}</a>`;
+        }
+    }
 })();
+
